117f39fbb8c6da8e348df4c59da4416e7b47ba50
RichardRoda <richard_roda@hotmail.com>
2025-12-22 12:26:01 -0500

    Clean up mess left behind by Cursor: unused constants with a directive to ignore them removed manually.

--- src/display.rs
+++ src/display.rs
@@ -2,45 +2,20 @@ use crate::board::{Board, Cell, Position};
 use crate::game::{Game, GameState, Player};
 
 /// ANSI color codes for terminal output
-#[allow(dead_code)]
 mod colors {
     pub const RESET: &str = "\x1b[0m";
     pub const BOLD: &str = "\x1b[1m";
     
     // Colors
-    pub const BLACK: &str = "\x1b[30m";
-    pub const RED: &str = "\x1b[31m";
-    pub const GREEN: &str = "\x1b[32m";
     pub const YELLOW: &str = "\x1b[33m";
-    pub const BLUE: &str = "\x1b[34m";
-    pub const MAGENTA: &str = "\x1b[35m";
     pub const CYAN: &str = "\x1b[36m";
-    pub const WHITE: &str = "\x1b[37m";
     
-    // Background colors
-    pub const BG_BLACK: &str = "\x1b[40m";
-    pub const BG_RED: &str = "\x1b[41m";
-    pub const BG_GREEN: &str = "\x1b[42m";
-    pub const BG_YELLOW: &str = "\x1b[43m";
-    pub const BG_BLUE: &str = "\x1b[44m";
-    pub const BG_MAGENTA: &str = "\x1b[45m";
-    pub const BG_CYAN: &str = "\x1b[46m";
-    pub const BG_WHITE: &str = "\x1b[47m";
     
     // Bright colors
     pub const BRIGHT_BLACK: &str = "\x1b[90m";
-    pub const BRIGHT_RED: &str = "\x1b[91m";
     pub const BRIGHT_GREEN: &str = "\x1b[92m";
-    pub const BRIGHT_YELLOW: &str = "\x1b[93m";
-    pub const BRIGHT_BLUE: &str = "\x1b[94m";
     pub const BRIGHT_MAGENTA: &str = "\x1b[95m";
-    pub const BRIGHT_CYAN: &str = "\x1b[96m";
-    pub const BRIGHT_WHITE: &str = "\x1b[97m";
-    
-    // Bright background colors
-    pub const BG_BRIGHT_BLACK: &str = "\x1b[100m";
-    pub const BG_BRIGHT_GREEN: &str = "\x1b[102m";
-    pub const BG_BRIGHT_YELLOW: &str = "\x1b[103m";
+    pub const BRIGHT_WHITE: &str = "\x1b[97m"; 
 }
 
 /// Color configuration
